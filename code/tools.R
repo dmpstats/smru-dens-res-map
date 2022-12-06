d8bcea7926f216342d1529d017fc71f10b08353f
Statsgeek <statsgeeknz@googlemail.com>
2022-12-06 08:35:47 +0000

    Update tools.R
    Inclusion of converters to get SE from CIs

--- code/tools.R
+++ code/tools.R
@@ -12,3 +12,32 @@ logSigma <- function(mu, sigma){
   sqrt(log(1+sigma^2/mu^2))
   
 }
+
+
+
+# SE from CI ----------------------------------------------------------------------------------
+
+  SEfromCI <- function(mu, lower, upper){
+    
+    logSE <- (log(upper) - log(lower))/(2*1.96)
+    
+    sigma <- sqrt((exp(logSE^2)-1)*mu^2)
+    
+    sigma
+      
+  }
+
+
+# CI sampler ----------------------------------------------------------------------------------
+
+  sampleLN <- function(mu, sigma, nSamp){
+    
+    if(mu == 0){
+      outSamp <- rep(0, nSamp)
+      } else {
+    outSamp <- rlnorm(nSamp, logMu(mu, sigma), logSigma(mu, sigma))
+      }
+    
+    outSamp
+    
+  }
