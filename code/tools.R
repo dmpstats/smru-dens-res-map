b51acb7801e7effdab8a59fc05fb5e64b45aa148
statsgeeknz <statsgeeknz@gmail.com>
2024-04-09 23:27:40 +0100

    Included wrapper function for batch fits

--- code/tools.R
+++ code/tools.R
@@ -78,3 +78,101 @@ gamFitTweedie <- function(inData, inP, inRES){
   outData
   
 }
+
+
+
+# overarching function for batch submission -----------------------------------------------------------------------
+
+
+
+fitFun <- function(speciesName, locationName, nBoot = 200, inData = workingData, inRES = workingRES, modelForm = "tweedie"){
+  
+  dataList <- split(inData, inData$blockID)
+  
+  set.seed(345)
+  
+  sampleList <- lapply(dataList, function(q){sampleLN(q$Density[1], q$workingSE[1], nBoot)})
+  
+  sampleDF <- plyr::ldply(sampleList) %>%
+    rename(Survey = .id)
+  
+  speciesSamples <- inData %>% left_join(sampleDF)
+  
+  speciesSamples <- speciesSamples %>% 
+    select(-Density) %>%
+    pivot_longer(names_to = "sampleID", values_to = "Density", V1:last_col()) 
+  
+  speciesList <- split(speciesSamples, speciesSamples$sampleID)
+  
+  plan(multicore, workers = 2)
+  
+  modelFit <- function(inList){
+    
+    p <- progressor(steps = length(inList))
+    
+    if(modelForm == "tweedie"){  
+      
+      future_map(inList, \(x) {p(); gamFitTweedie(x, inP = 1.2, inRES = data.frame(RES = seq(0, 1, by = 0.01)))})
+      
+    } else {
+      
+      future_map(inList, \(x) {p(); gamFit(x, inRES = data.frame(RES = seq(0, 1, by = 0.01)))})
+      
+    }
+    
+  }
+  
+  
+  with_progress({
+    fittedList <- modelFit(speciesList)
+  })
+  
+  
+  fittedDF <- fittedList %>%
+    bind_rows()
+  
+  fittedMatrix <- matrix(fittedDF$Pred, ncol = nBoot)
+  
+  bootEsts <- t(apply(fittedMatrix, 1, function(q){quantile(q, probs = c(0.025, 0.5, 0.975))}))
+  
+  bootSE <- apply(fittedMatrix, 1, sd)
+  
+  resFits <- as.data.frame(bootEsts) %>%
+    mutate(RES = seq(0, 1, by = 0.01), SE = bootSE) %>%
+    rename(lower = `2.5%`, med = `50%`, upper = `97.5%`) %>%
+    mutate(med = ifelse(med < 0, min(abs(med)), med),
+           CV = SE/med) %>%
+    #CV = ifelse(CV > 2, 2, CV)) %>%
+    arrange(RES)
+  
+  plottingDF <- resFits
+  
+  bootPlot <- ggplot(plottingDF) +
+    ggthemes::theme_fivethirtyeight() +
+    geom_line(aes(RES, med), size = 2, alpha = 0.7, col = "purple") +
+    geom_ribbon(aes(x = RES, ymin = lower, ymax = upper), fill = "purple", alpha = 0.2) +
+    ggtitle("Density as function of RES", paste0(speciesName, " whale: bootstrapped monotone spline fits"))
+  
+  ggsave(plot = bootPlot, here(paste0("docs/images/", speciesName, "_", locationName, "_bootplot_", nBoot, ".png")), units = "cm", width = 30, height = 20)
+  saveRDS(plottingDF, here(paste0("data/plotting components/", speciesName, "_", locationName, "_plotElements",".rds")))
+  
+  
+  # Create RES grid predictions -----------------------------------------------------------------
+  
+  resFits <- resFits %>% select(med, RES, CV) %>%
+    rename(PredDensity = med) %>%
+    mutate(RES = round(RES, 2))
+  
+  predictionOutput <- inRES %>% left_join(resFits, by = "RES")
+  
+  predictionOutput <- predictionOutput %>%
+    mutate(PredDensity = ifelse(RES == 0, 0, PredDensity),
+           CV = ifelse(RES == 0, NA, CV))
+  
+  summary(predictionOutput)
+  
+  write.csv(predictionOutput, file = here(paste0("data/predictions/", speciesName, "_", locationName, "_predictions.csv")), row.names = F)
+  
+}
+
+
